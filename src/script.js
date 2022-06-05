8acd5fe9ab095e609d2b015138e5a9167aea0790
Ritulja <104376569+Ritulja@users.noreply.github.com>
2022-06-05 21:15:31 +0100

    added icon

--- src/script.js
+++ src/script.js
@@ -35,6 +35,12 @@ function displayWeatherCondition(response) {
   document.querySelector("#wind").innerHTML = Math.round(
     response.data.wind.speed
   );
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 function searchCity(city) {
   let apiKey = "2881e3d61ec2ba64d89fe66a778a8135";
